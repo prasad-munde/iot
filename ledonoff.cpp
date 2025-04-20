41c5c442fd312eacbf71c1ce2339c6eed73db03d
prasad-munde <163027732+prasad-munde@users.noreply.github.com>
2025-04-20 19:41:39 +0530

    Create ledonoff.cpp

---
+++ ledonoff.cpp
@@ -0,0 +1,16 @@
+int redPin = 12;
+
+void setup(){
+    pinMode(redPin,OUTPUT);
+    
+    
+    
+}
+
+void loop(){
+    digitalWrite(redPin,HIGH);
+    delay(500);
+    digitalWrite(redPin,LOW);
+    delay(500);
+    
+}
