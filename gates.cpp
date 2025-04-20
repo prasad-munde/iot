a36c73570e43c2fa5f9dff14d729d96d73ec8443
prasad-munde <163027732+prasad-munde@users.noreply.github.com>
2025-04-20 23:37:48 +0530

    Update gates.cpp

--- gates.cpp
+++ gates.cpp
@@ -1,1 +1,30 @@
-
+int button1 = 2;
+int button2 = 3;
+int led = 13;
+void setup() {
+ pinMode(button1, INPUT);
+ pinMode(button2, INPUT);
+ pinMode(led, OUTPUT);
+}
+void loop() {
+ int a = digitalRead(button1);
+ int b = digitalRead(button2);
+//For the AND GATE
+ if (a == HIGH && b == HIGH) {
+ digitalWrite(led, HIGH);
+ } else {
+ digitalWrite(led, LOW);
+ }
+//For The OR GATE
+if (a == HIGH || b == HIGH) {
+ digitalWrite(led, HIGH);
+} else {
+ digitalWrite(led, LOW);
+}
+//For the XOR GATE
+if (a != b) { // XOR logic
+ digitalWrite(led, HIGH);
+} else {
+ digitalWrite(led, LOW);
+}
+}
